75edac54e2181c97fb638da1b81eb1de007d1f64
yanislammari <yanis.lammari937@gmail.com>
2025-04-20 22:45:13 +0200

    add user schemas

---
+++ src/schemas/database/user.database.schema.ts
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+import Role from "../../models/enums/role";
+
+const UserDatabaseSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  tel: {
+    type: String,
+    required: true,
+    match: /^[\d+\-()\s]+$/
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 20
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 20
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 10,
+    default: 5
+  },
+  role: {
+    type: String,
+    enum: Object.values(Role),
+    required: true
+  },
+  profilePictureUrl: {
+    type: String,
+    required: false
+  },
+  bio: {
+    type: String,
+    maxlength: 100,
+    required: false
+  },
+  profession: {
+    type: String,
+    maxlength: 30,
+    required: false
+  },
+  address: {
+    type: String,
+    maxlength: 50,
+    required: false
+  },
+  city: {
+    type: String,
+    maxlength: 30,
+    required: false
+  },
+  country: {
+    type: String,
+    maxlength: 20,
+    required: false
+  },
+  postalCode: {
+    type: String,
+    maxlength: 10,
+    required: false
+  }
+});
+
+UserDatabaseSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+UserDatabaseSchema.set("toJSON", { 
+  virtuals: true 
+});
+
+UserDatabaseSchema.set("toObject", { 
+  virtuals: true 
+});
+
+export default UserDatabaseSchema;
---
+++ src/schemas/request/user.request.schema.ts
@@ -0,0 +1,21 @@
+import joi from "joi";
+import Role from "../../models/enums/role";
+import UserRequestDto from "../../models/entities/user/dto/user.request.dto";
+
+const UserRequestSchema = joi.object<UserRequestDto>({
+  email: joi.string().email().required(),
+  tel: joi.string().pattern(/^[\d+\-()\s]+$/).required(),
+  firstName: joi.string().min(2).max(20).required(),
+  lastName: joi.string().min(2).max(20).required(),
+  age: joi.number().min(0).max(100).required(),
+  role: joi.string().valid(...Object.values(Role)).required(),
+  profilePictureUrl: joi.string().uri().optional(),
+  bio: joi.string().max(100).optional(),
+  profession: joi.string().max(30).optional(),
+  address: joi.string().max(50).optional(),
+  city: joi.string().max(30).optional(),
+  country: joi.string().max(20).optional(),
+  postalCode: joi.string().max(10).optional(),
+});
+
+export default UserRequestSchema;
